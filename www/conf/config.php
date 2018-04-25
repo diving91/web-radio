7daa4213f72a5c70fe992fc431de6184ab9ec1b3
Diving91 <avhs@free.fr>
2018-04-26 00:37:06 +0200

    Add config for jeedom scenario callback

--- www/conf/config.php
+++ www/conf/config.php
@@ -6,11 +6,12 @@ $config = [
 			"vol"		=> 70,
 			"localTTS"	=> true,
 			"weatherUrl"	=> 'https://www.prevision-meteo.ch/services/json/#your city name#',
-			"jeedomUrl"	=> 'http://192.168.1.108/core/api/jeeApi.php?apikey=#your Jeedom API key#&type=interact&query=#your interaction query#'],
+			"jeedomUrl"	=> 'http://#your jeedom IP#/core/api/jeeApi.php?apikey=#your Jeedom API key#&type=interact&query=#your interaction query#'],
 	"cron" => [	"path"		=> '/srv/www/home.fr/public/conf/cron.txt'],
 	"station" => [	"path"		=> '/srv/www/home.fr/public/station/station.txt',
 			"local"		=> '/srv/www/home.fr/public/conf/clocher-x12-SF.mp3',
 			"default"	=> 'http://stream1.chantefrance.com/stream_chante_france.mp3'],
-	"snooze" => [	"delay"		=> '+5 minutes']	
+	"snooze" => [	"delay"		=> '+5 minutes'],
+	"jeedom" => [	"scenario"	=> 'http://#your jeedom IP#/core/api/jeeApi.php?apikey=#your Jeedom API key#&type=scenario&id="ID of scenario to call#&action=start']
  ];
 
